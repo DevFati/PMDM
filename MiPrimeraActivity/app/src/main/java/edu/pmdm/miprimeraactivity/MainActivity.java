d3e66ee09344b70d33208859afe9930f4fdfa780
Fatima <fatima.mortahilchachou@riberadeltajo.es>
2024-09-22 15:24:57 +0200

    subida ej

--- MiPrimeraActivity/app/src/main/java/edu/pmdm/miprimeraactivity/MainActivity.java
+++ MiPrimeraActivity/app/src/main/java/edu/pmdm/miprimeraactivity/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.pmdm.miprimeraactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View .OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+       // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         //mantener aqui nuestro código
 
+        Button b=(Button)findViewById(R.id.button);
+        b.setOnClickListener(this);
 
+    }
 
+    @Override
+    public void onClick(View v) {
+        TextView t=(TextView)findViewById(R.id.textView2);
+        t.setText("Pulsado");
     }
 }
